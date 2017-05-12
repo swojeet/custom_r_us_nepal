4d07d2a9677f6387358c205fca51eabb94b135ce
swojeet <swojeetkayastha@gmail.com>
2017-05-12 17:30:07 +0545

    Update devise permitted fields and add all fields for signing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :address, :city, :country, :phone_number, :date_of_birth])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :address, :city, :country, :phone_number, :date_of_birth])
+  end
+
+  private
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+
+  GENDER_TYPES = ["Male","Female","Not Telling"]
 end
